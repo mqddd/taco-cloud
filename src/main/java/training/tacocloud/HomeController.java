dbe8b436edf6e4db4a46c93afcb1d856efb388ed
mqddd <meghdadd78@gmail.com>
2021-04-14 16:10:14 +0430

    home page created and configued

---
+++ src/main/java/training/tacocloud/HomeController.java
@@ -0,0 +1,13 @@
+package training.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String home(){
+        return "home";
+    }
+}
